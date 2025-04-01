c0e830faa52be2d6f4b1b222852d00b97890ce8c
coblove0 <coblove0@gmail.com>
2025-04-01 08:21:44 +0400

    feat: настройка ts и eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser
+    },
+    plugins: { "@typescript-eslint": ts },
+    rules: {
+      "camelcase": ["error", { "properties": "always" }]
+    }
+  }
+];
