bea51f0ee37e6d098066936d7a7afa0ea44ee9bc
coblove0 <coblove0@gmail.com>
2025-04-07 22:32:16 +0400

    antd-button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MyTab from './components/MyTab/MyTab';
+import MyButton from './components/MyButton/MyButton';
 
 function App() {
   return (
     <div>
       <MyTab />
+      <MyButton />
     </div>
     // <div>
     //   <h1>Добро пожаловать в Vite + React!</h1>
