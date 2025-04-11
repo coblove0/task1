7cf5e4a46f25864b0428a7d8b327318db71e1969
coblove0 <coblove0@gmail.com>
2025-04-11 19:19:23 +0400

    vite styled components

---
+++ vite/src/components/MyCard/MyCard.tsx
@@ -0,0 +1,15 @@
+import EmotionButton from 'components/EmotionButton/EmotionButton';
+import TailwindContent from 'components/TailwindContent/TailwindContent';
+import TailwindTitle from 'components/TailwindTitle/TailwindTitle';
+
+function MyCard() {
+  return (
+    <div>
+      <TailwindTitle />
+      <TailwindContent />
+      <EmotionButton />
+    </div>
+  );
+}
+
+export default MyCard;
---
+++ vite/src/components/TailwindContent/TailwindContent.tsx
@@ -0,0 +1,7 @@
+import './TailwindContent.css';
+
+function TailwindContent() {
+  return <div className="text-xl font-medium text-black dark:text-white bg-blue-400">Контент</div>;
+}
+
+export default TailwindContent;
