da9848558ddfa3697c067834e257760a1cd4cc8b
coblove0 <coblove0@gmail.com>
2025-04-15 21:50:20 +0400

    clear vite project

---
+++ marketplace/eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  prettierConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: { 
+      '@typescript-eslint': ts,
+       prettier,
+    },
+    rules: {
+      camelcase: ['error', { properties: 'always' }],
+      "prettier/prettier": [
+        'error',
+        {
+          endOfLine: 'auto',
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 100,
+          tabWidth: 2,
+          semi: true,
+        },
+      ],
+    },
+  },
+];
