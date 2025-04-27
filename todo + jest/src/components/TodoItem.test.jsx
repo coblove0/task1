0f2b4aedd12374164e16d328b7de5edf70510fce
coblove0 <coblove0@gmail.com>
2025-04-27 15:01:21 +0400

    todoitem.test.jsx

--- todo + jest/src/components/TodoItem.test.jsx
+++ todo + jest/src/components/TodoItem.test.jsx
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import TodoItem from './TodoItem';
 
 describe('TodoItem', () => {
+  const updatedTodo = { ...mockTodo, completed: true };
   const mockTodo = {
     id: 1,
     text: 'Test todo',
@@ -22,5 +23,41 @@ describe('TodoItem', () => {
 
     expect(screen.getByText('Test todo')).toBeInTheDocument();
     expect(screen.getByRole('checkbox')).not.toBeChecked();
+  });
+
+  test('item deleting calls onDelete()', () => {
+    render(
+      <TodoItem 
+        todo={mockTodo} 
+        onToggle={mockOnToggle} 
+        onDelete={mockOnDelete} 
+      />
+    );
+    
+    const deleteButton = screen.getByTestId('delete-1');
+    fireEvent.click(deleteButton);
+    expect(mockOnDelete).toHaveBeenCalled();
+  });
+
+  test('status change works', () => {
+    const { rerender } = render(
+      <TodoItem 
+        todo={mockTodo} 
+        onToggle={mockOnToggle} 
+        onDelete={mockOnDelete} 
+      />
+    );
+
+    const toggleButton = screen.getByTestId('toggle-1');
+    fireEvent.click(toggleButton);
+    rerender(
+    <TodoItem 
+      todo={updatedTodo} 
+      onToggle={mockOnToggle} 
+      onDelete={mockOnDelete} 
+    />
+  );
+    expect(mockOnToggle).toHaveBeenCalledWith(mockTodo.id);
+    expect(toggleButton).toBeChecked();
   });
 });
